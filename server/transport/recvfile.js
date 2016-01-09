ceccdeae22193562cfe7422dba8b4d65de4771ab
kevinxia <xxiiaass@sina.com>
2016-01-09 10:47:18 +0800

    backup

--- server/transport/recvfile.js
+++ server/transport/recvfile.js
@@ -25,48 +25,56 @@ var dealblog = function (files) {
     return new Promise((resolve, reject)=>{
         co(function* () {
             var blog = yield unam(files.upload.path);
-            var imgReg = 
+            var imgReg = /\[!.+\]\((?!\/images\/).*?\)/;
             var eventcallback = function (imgfile){
-                if(blog.contentfunc().indexOf(imgfile.upload.name) == -1)
-                    return;
-                const savePath = path.join(__dirname, "../../client/images/blog/");
-                var oldname = path.basename(imgfile.upload.name);
-                var savename = oldname;
-                while(1){
-                    var exist = yield toPro(path.join(savePath, savename ), fs.exists);
-                    if(exist[0]){
-                        savename = 'T-'+savename;
-                        continue;
-                    }else{
-                        var ret = yield toPro(files.upload.path, path.join(savePath, savename), fs.rename);
-                        if(ret[0])
-                            reject(ret);
-                        break;
+                co(function* () {
+                    console.log(blog.contentfunc().indexOf(imgfile.upload.name));
+                    if(blog.contentfunc().indexOf(imgfile.upload.name) == -1)
+                        return;
+                    const savePath = path.join(__dirname, "../../client/images/blog/");
+                    var oldname = path.basename(imgfile.upload.name);
+                    var savename = oldname;
+                    while(1){
+                        var exist = yield toPro(path.join(savePath, savename ), fs.exists);
+                        if(exist[0]){
+                            savename = 'T-'+savename;
+                            continue;
+                        }else{
+                            var ret = yield toPro(files.upload.path, path.join(savePath, savename), fs.rename);
+                            if(ret[0])
+                                reject(ret);
+                            break;
+                        }
                     }
-                }
-                var imgurl = path.join('/images/blog/', savename);
-                var reg = new RegExp(imgfile.upload.name, "g");
-                blog.contentfunc(blog.contentfunc().replace(reg, imgurl));
-            });
+                    var imgurl = path.join('/images/blog/', savename);
+                    var reg = new RegExp(imgfile.upload.name, "g");
+                    blog.contentfunc(blog.contentfunc().replace(reg, imgurl));
+                    if(blog.contentfunc().test(imgReg))
+                        return ;
+                    else
+                        emitter.removeListener('comepic', eventcallback);
+                });
+            };
 
             var saveblogfile = function () {
-                blog.save();
-                var today = new Date();
-                var newfilepath = path.join(savepath, 'date-' + today.getFullYear() + today.getMonth());
-                var exist = yield toPro(newfilepath, fs.exists);
-                if(!exist[0]){
-                    var fail = yield toPro(newfilepath, fs.mkdir);
-                    if(fail)reject(fail);
-                }
+                co(function* () {
+                    blog.save();
+                    var today = new Date();
+                    var newfilepath = path.join(savepath, 'date-' + today.getFullYear() + today.getMonth());
+                    var exist = yield toPro(newfilepath, fs.exists);
+                    if(!exist[0]){
+                        var fail = yield toPro(newfilepath, fs.mkdir);
+                        if(fail)reject(fail);
+                    }
 
-                var newname = path.join(newfilepath, files.upload.name);
-                var ret = yield toPro(files.upload.path, newname, fs.rename);
-                if(ret[0])
-                    reject(ret);
-                resolve('ok');
+                    var newname = path.join(newfilepath, files.upload.name);
+                    var ret = yield toPro(files.upload.path, newname, fs.rename);
+                    if(ret[0])
+                        reject(ret);
+                    resolve('ok');
+                });
             }
-
-            if(/(\[!\S*?\])\(\S*?\)/.test(blog.contentfunc())){
+            if(imgReg.test(blog.contentfunc())){
                 emitter.on('comepic',eventcallback);
             }
             else
@@ -94,7 +102,6 @@ var recvfile = function(req){
             form.uploadDir = path.join(__dirname, './tmpfiles');
 
             form.parse(req, (err, fields, files)=>{
-                console.log(files);
                 if (err)
                     reject(err);
                 var extname = path.extname(files.upload.path);
